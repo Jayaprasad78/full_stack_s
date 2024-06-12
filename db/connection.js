b0dec286fc6a8e0aedc53204296b46a33725a5e1
JAYAPRASAD B <127640948+Jayaprasad78@users.noreply.github.com>
2024-06-12 19:26:08 +0530

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,9 @@
 const M=require('mongoose')
-M.connect('mongodb+srv://jayaprasadb718:1FegFVGI622NN4hw@cluster0.vwyhndh.mongodb.net/my_database?retryWrites=true&w=majority&appName=Cluster0')
+M.connect('mongodb+srv://jayaprasadb718:1FegFVGI622NN4hw@cluster0.vwyhndh.mongodb.net/my_database?retryWrites=true&w=majority&appName=Cluster0',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000 
+})
 .then( ()=>{
     console.log("server is connected to database")
 })
